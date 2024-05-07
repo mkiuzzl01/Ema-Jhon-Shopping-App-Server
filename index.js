cb10f99a5e99fda48a9fec5bec2f02e5d2ed1aba
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-05-07 20:07:06 +0600

    pagination feture add in this server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,9 +11,7 @@ app.use(express.json());
 
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rbychrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,11 +27,36 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const productCollection = client.db('emaJohnDB').collection('products');
+    const productCollection = client.db('Ema-jhonDB').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+        const result = await productCollection.find()
+        .skip(page*size)
+        .limit(size)
+        .toArray();
         res.send(result);
+    })
+
+    app.get('/productsCount', async (req,res)=>{
+      const count = await productCollection.estimatedDocumentCount()
+      res.send({count});
+    })
+    
+    app.post('/productsById', async (req,res)=>{
+      const ids = req.body;
+      console.log(ids);
+      const idsWithObjectId = ids.map(id=> new ObjectId(id));
+      const query = {
+        _id:{
+          $in:idsWithObjectId
+        }
+      }
+      console.log(idsWithObjectId);
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+      // console.log(result);
     })
 
     // Send a ping to confirm a successful connection
